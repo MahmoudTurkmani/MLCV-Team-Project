4f7b19bb108be7fc617ece0015c2e3803d833b50
sr <sr@arch.x270>
2026-07-13 11:35:12 +0200

    fixed slurm script for new venv, added gnu screen script as alternative for cpu runs in background

---
+++ MAE/screen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+module load release/2026 GCCcore/14.3.0 CUDA/12.1.1 Python/3.13.5 FFmpeg/7.1.2
+
+cd /home/mari880e/MLCV-Team-Project/MAE
+
+source ./venv/bin/activate
+
+python train_birdmae.py "$@"
